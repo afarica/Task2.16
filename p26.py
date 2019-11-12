a9fc91bf7495639f2f6e37be0a1cee09a956e698
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 06:43:55 +0600

    Update p26.py

--- p26.py
+++ p26.py
@@ -1,3 +1,7 @@
+# If you were on the moon now, your weight will be 16,5% of your earth weight.
+# To calculate it you have to multiple to 0,165. If next 15 years your weight will
+# increase 1 kg each year. What will be your weight each year on the moon next
+# 15 years?
 your_weight=float(input("Enter your weight:"))
 on_the_moon=your_weight*0.165
 for x in range(0,16):
